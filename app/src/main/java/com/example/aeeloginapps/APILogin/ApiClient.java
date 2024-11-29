e9ba6f80258fd2fc031a3584e0c4eac8b1306b6b
Raul 23 <raulgutierrez.23@campuscamara.es>
2024-11-29 09:50:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aeeloginapps/APILogin/ApiClient.java
@@ -0,0 +1,211 @@
+package com.example.aeeloginapps.APILogin;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ApiClient {
+    private static final String API_URL = "https://sandbox.phpulse.es/index.php?action=usuarios";
+
+    public static boolean validarUser(String correo, String pass) {
+        Usuario[] usuarios = obtenerUsuarios();
+
+        for (Usuario u : usuarios) {
+            if (u.getEmail().equals(correo) && u.getPassword().equals(pass)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Usuario[] obtenerUsuarios() {
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                JSONArray jsonArray = new JSONArray(response);
+                Usuario[] usuarios = new Usuario[jsonArray.length()];
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+
+                    Usuario usuario = new Usuario();
+                    usuario.setId(obj.getString("id"));
+                    usuario.setNombre(obj.getString("nombre"));
+                    usuario.setEmail(obj.getString("email"));
+                    usuario.setPassword(obj.getString("password"));
+                    usuario.setCreado_en(obj.getString("creado_en"));
+
+                    System.out.println("Email: " + usuario.getEmail());
+                }
+                return usuarios;
+            } else {
+                System.out.println("Error: " + conn.getResponseCode());
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String obtenerUsuarioPorId(int id) {
+        try {
+            URL url = new URL(API_URL + "&id=" + id);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+
+
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void crearUsuario(String nombreUsuario, String email, String contraseña) {
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+            String jsonInputString = String.format("{\"entidad\":\"usuarios\",\"nombre\":\"%s\", \"email\":\"%s\", \"password\":\"%s\"}", nombreUsuario, email, contraseña);
+            System.out.println(jsonInputString);
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            if (conn.getResponseCode() == 200) {
+                System.out.println("Usuario creado con éxito.");
+            } else {
+                System.out.println("Error al crear usuario: " + conn.getResponseCode());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void actualizarUsuario(int id, String nombreUsuario, String email) {
+        try {
+            URL url = new URL(API_URL + "&id=" + id);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("PUT");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+            String jsonInputString = String.format("{\"entidad\":\"usuarios\",\"nombre\":\"%s\", \"email\":\"%s\"}", nombreUsuario, email);
+            System.out.println(jsonInputString);
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            if (conn.getResponseCode() == 200) {
+                System.out.println("Usuario actualizado con éxito.");
+
+            } else {
+                System.out.println("Error al actualizar usuario: " + conn.getResponseCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void eliminarUsuario(int id) {
+        try {
+            URL url = new URL(API_URL + "&id=" + id);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("DELETE");
+            if (conn.getResponseCode() == 200) {
+                System.out.println("Usuario eliminado con éxito.");
+            } else {
+                System.out.println("Error al eliminar usuario: " + conn.getResponseCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Usuario {
+    private String id;
+    private String nombre;
+    private String email;
+    private String password;
+    private String creado_en;
+
+    public Usuario(String id, String nombre, String email, String password, String creado_en) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        this.creado_en = creado_en;
+    }
+
+    public Usuario() {
+    }
+
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreado_en() {
+        return creado_en;
+    }
+
+    public void setCreado_en(String creado_en) {
+        this.creado_en = creado_en;
+    }
+}
---
+++ app/src/main/java/com/example/aeeloginapps/APILogin/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.example.aeeloginapps.APILogin
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.aeeloginapps.R
+import com.example.aeeloginapps.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun btnLoginOnClick(view: View) {
+        val correo=binding.correo.text.toString()
+        val pass = binding.pass.text.toString()
+
+        ApiClient.validarUser(correo,pass)
+    }
+    fun etARegOnClick(view: View) {}
+}
